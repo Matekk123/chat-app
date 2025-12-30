083a8f780f3615853b4b0192c77210749e570f8e
Matekk123 <balassa.mate2008@gmail.com>
2025-12-30 19:08:35 +0100

    Refactor server to support room-based messaging

--- server.js
+++ server.js
@@ -5,33 +5,22 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-// Itt tároljuk el az üzeneteket, amíg a szerver fut
-let uzenetElőzmények = [];
-
 io.on('connection', (socket) => {
-    console.log('Új csatlakozó: ' + socket.id);
-
-    // Amint valaki belép, azonnal elküldjük neki a régi üzeneteket
-    socket.emit('korabbi-uzenetek', uzenetElőzmények);
-
-    socket.on('uzenet', (adat) => {
-        // Hozzáadjuk az új üzenetet a listához
-        uzenetElőzmények.push(adat);
-        
-        // Csak az utolsó 50 üzenetet őrizzük meg, hogy ne teljen be a memória
-        if (uzenetElőzmények.length > 50) {
-            uzenetElőzmények.shift(); 
-        }
-
-        // Mindenkinek kiküldjük az új üzenetet
-        io.emit('uzenet', adat);
+    
+    socket.on('join-room', (roomId) => {
+        socket.join(roomId);
     });
 
-    // WebRTC Hívás jelzések
+    socket.on('privat-uzenet', (adat) => {
+        // Csak abba a szobába küldjük, ahova szánták
+        io.to(adat.roomId).emit('uj-uzenet', adat);
+    });
+
+    // WebRTC jelzések (szintén szobához köthetők majd)
     socket.on('hivas-ajanlat', (data) => socket.broadcast.emit('hivas-ajanlat', data));
     socket.on('hivas-valasz', (data) => socket.broadcast.emit('hivas-valasz', data));
     socket.on('ice-candidate', (data) => socket.broadcast.emit('ice-candidate', data));
 });
 
-const PORT = 3000;
-http.listen(PORT, () => console.log(`Szerver fut: http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log('Szerver fut...'));
